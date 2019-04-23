598f1aeda54f293d371f11bd25725210ac38238a
lengai <371522155@qq.com>
2019-04-23 20:22:39 +0800

    add fields for hosts table

--- app/Models/Host.php
+++ app/Models/Host.php
@@ -7,14 +7,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Host extends Model
 {
+    /**获取收录记录总数
+     * @return mixed
+     */
     public function getAmountRecordAttribute()
     {
         return Url::where('host_id', $this->host_id)->count();
     }
 
+
+    /**获取排名记录总数
+     * @return mixed
+     */
     public function getRankRecordAttribute()
     {
         return HostRank::where('host_id', $this->host_id)->count();
+    }
+
+    /**获取查询状态
+     * @return string 查询状态
+     */
+    public function getZtAttribute()
+    {
+        $zt = '';
+        switch ($this->status) {
+            case 1:
+                $zt = "估计10分钟完成";
+                break;
+            case 2:
+                $zt = "已经完成";
+                break;
+        }
+
+        return $zt;
     }
 
 }
